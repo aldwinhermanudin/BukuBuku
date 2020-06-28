7a65f601ce557f4c60c8e4e4f949e36cc5db4fee
Aldwin Hermanudin <aldwin@hermanudin>
2020-06-28 08:34:50 +0100

    working kernel spi driver with device tree

--- experimental/phase0/kernelspace-driver/src/simple-spi/simple-spi.c
+++ experimental/phase0/kernelspace-driver/src/simple-spi/simple-spi.c
@@ -5,56 +5,39 @@
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
 
-// static int ws2in13b_set_reg(struct device *dev, unsigned char address,
-// 				unsigned char data)
-// {
-// 	struct spi_device *spi = to_spi_device(dev);
-// 	unsigned char buf[2];
-
-// 	/* MSB must be '1' to indicate write */
-// 	buf[0] = address | 0x80;
-// 	buf[1] = data;
-
-// 	return spi_write_then_read(spi, buf, 2, NULL, 0);
-// }
-
-// static int ws2in13b_get_reg(struct device *dev, unsigned char address,
-// 				unsigned char *data)
-// {
-// 	struct spi_device *spi = to_spi_device(dev);
-
-// 	*data = address & 0x7f;
-
-// 	return spi_write_then_read(spi, data, 1, data, 1);
-// }
-
 static int ws2in13b_probe(struct spi_device *spi){
-   int ret = 0;
+	int ret = 0;
 	unsigned char buf;
 
 	spi->mode = SPI_MODE_0;
 	spi->bits_per_word = 8;
 	spi->max_speed_hz = 1*1000*1000;
-	
-   ret = spi_setup(spi);
 
-   buf = 0x04;
-   spi_write(spi, &buf,1);
+	printk(KERN_INFO "Setting-up SPI device\n");
+	ret = spi_setup(spi);
+	printk(KERN_INFO "Succeeded to register SPI device\n");
 
-   if (ret < 0){
-      return ret;
-   }
+	buf = 0x04;
+	spi_write(spi, &buf,1);
 
-	return 0;
+	if (ret < 0){
+		printk(KERN_ALERT "Failed to register SPI device\n");
+		return ret;
+	}
+
+	printk(KERN_INFO "Succeeded to setup SPI device\n");
+
+return 0;
 }
 
-// static int ws2in13b_remove(struct spi_device *spi)
-// {
-// 	// struct rtc_device *rtc = platform_get_drvdata(spi);
+static int ws2in13b_remove(struct spi_device *spi)
+{
+	printk(KERN_INFO "Removing SPI device\n");
+	// struct rtc_device *rtc = platform_get_drvdata(spi);
 
-// 	// rtc_device_unregister(rtc);
-// 	return 0;
-// }
+	// rtc_device_unregister(rtc);
+	return 0;
+}
 
 static struct spi_driver ws2in13b_driver = {
 	.driver = {
@@ -62,17 +45,20 @@ static struct spi_driver ws2in13b_driver = {
 		.owner	= THIS_MODULE,
 	},
 	.probe	 = ws2in13b_probe,
-	// .remove  = ws2in13b_remove,
+	.remove  = ws2in13b_remove,
 };
 
 static __init int ws2in13b_init(void)
-{
+{	
+	printk(KERN_INFO "Registering SPI device\n");
 	return spi_register_driver(&ws2in13b_driver);
 }
+
 module_init(ws2in13b_init);
 
 static __exit void ws2in13b_exit(void)
 {
+	printk(KERN_INFO "De-registering SPI device\n");
 	spi_unregister_driver(&ws2in13b_driver);
 }
 module_exit(ws2in13b_exit);
